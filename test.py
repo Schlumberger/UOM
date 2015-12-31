12d6c79a8b13b5817165f32cd008ded3bbb0f25b
VelizarVESSELINOV <VV@Velizar-MacBook-Pro.local>
2015-12-31 15:04:53 -0800

    Test function enhancement

--- test.py
+++ test.py
@@ -1,43 +1,35 @@
 """Test functions."""
+from unittest import TestCase, main
+
 from unit_alias import unit_alias, unit_alias_dict
 from uom import base_unit, convert
 
 
-def test():
-    """Test function."""
-    tests = {
-        "ft/h => ft/s": [1, "ft/h", "ft/s", 0.0002777777777777778],
-        "m => ft": [10, "m", "ft", 32.80839895013123],
-        "ft => m": [30, "ft", "m", 9.144],
-        "degC => degF": [22.4, "degC", "degF", 72.32]
-    }
+class UOM_test(TestCase):
+    """Test class."""
 
-    for k in sorted(tests):
-        print("Test conversion {0}".format(k))
-        print('=' * 80)
-        i = tests[k]
-        o = convert(i[0], i[1], i[2])
+    def test_conversion(self):
+        """Test conversion function."""
+        tests = {
+            "ft/h => ft/s": [1, "ft/h", "ft/s", 0.0002777777777777778],
+            "m => ft": [10, "m", "ft", 32.80839895013123],
+            "ft => m": [30, "ft", "m", 9.144],
+            "degC => degF": [22.4, "degC", "degF", 72.32]
+        }
 
-        if o == i[3]:
-            print("OK {0}({1} == {2})".format(k, o, i[3]))
-        else:
-            assert(False)
-            print("Error {0} ({1} <> {2}, diff {3} %)".format(k, o, i[3],
-                                                              abs(100 - o /
-                                                                  i[3] * 100)))
+        for k in sorted(tests):
+            i = tests[k]
+            o = convert(i[0], i[1], i[2])
 
-        print('=' * 80)
+            self.assertEqual(o, i[3])
 
-    for k in sorted(unit_alias_dict):
-        print("Test unit alias {0}".format(k))
-        print('=' * 80)
+    def test_unit_alis(self):
+        """Test unit aliases function."""
+        for k in sorted(unit_alias_dict):
+            unit = unit_alias(k)
+            base = base_unit(unit)
+            self.assertIsNotNone(base)
 
-        unit = unit_alias(k)
-        base = base_unit(unit)
-        print("Alias: {0}, Unit: {1}, Base unit: {2}".format(k, unit, base))
 
-        assert(base is not None)
-
-        print('=' * 80)
-
-test()
+if __name__ == '__main__':
+    main()
