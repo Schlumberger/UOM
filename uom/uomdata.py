4f63b1e925a20a2bc0d45d557c27ca938d69cbfc
Velizar VESSELINOV <velizar.vesselinov@gmail.com>
2016-01-02 22:07:54 -0800

    E231 fix

--- uom/uomdata.py
+++ uom/uomdata.py
@@ -2,4 +2,4 @@
 from pandas import read_csv
 from pkg_resources import resource_filename
 
-df_uom = read_csv(resource_filename('uom','uom.tsv'), sep='\t', index_col=0)
+df_uom = read_csv(resource_filename('uom', 'uom.tsv'), sep='\t', index_col=0)
