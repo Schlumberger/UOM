d7b3d23d9a1a980977e30e7db7cb7c387fd6a8f2
Velizar VESSELINOV <velizar.vesselinov@gmail.com>
2016-01-02 22:07:14 -0800

    better package resources management

--- uom/uomdata.py
+++ uom/uomdata.py
@@ -1,4 +1,5 @@
 """UOM DataFrame."""
 from pandas import read_csv
+from pkg_resources import resource_filename
 
-df_uom = read_csv("uom/uom.tsv", sep='\t', index_col=0)
+df_uom = read_csv(resource_filename('uom','uom.tsv'), sep='\t', index_col=0)
