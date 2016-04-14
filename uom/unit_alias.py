c56f7968287f65e73148b6323bbee8bc3e8494ba
Velizar VESSELINOV <velizar.vesselinov@gmail.com>
2016-04-14 16:04:29 -0700

    Example how to add unit aliases

--- uom/unit_alias.py
+++ uom/unit_alias.py
@@ -1,5 +1,6 @@
 """Unit alias dictionary."""
 
+# Alias: UOM
 unit_alias_dict = {
     "deg": "dega",
     "lbm/gal": "lbf/gal[US]",
@@ -9,7 +10,11 @@ unit_alias_dict = {
     "deg/100ft": "0.01 dega/ft",
     "feet": "ft",
     "FT": "ft",
-    "foot": "ft"
+    "foot": "ft",
+    "ft/ht": "ft/h",
+    "1/30 deg/m": "1/30 dega/m",
+    "1000 ft.lbf": "1000 lbf.ft",
+    "1000 lbf": "klbf"
 }
 
 
