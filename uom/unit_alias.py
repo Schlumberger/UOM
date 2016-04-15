4e65788cbd99ca0abb5b4bc2b8f6e37f34901810
Velizar VESSELINOV <velizar.vesselinov@gmail.com>
2016-04-14 21:19:27 -0700

    More units added

--- uom/unit_alias.py
+++ uom/unit_alias.py
@@ -2,19 +2,33 @@
 
 # Alias: UOM
 unit_alias_dict = {
-    "deg": "dega",
-    "lbm/gal": "lbf/gal[US]",
-    "gal/min": "gal[US]/min",
-    "c/min": "rpm",
-    "kft.lbf": "1000 lbf.ft",
-    "deg/100ft": "0.01 dega/ft",
-    "feet": "ft",
-    "FT": "ft",
-    "foot": "ft",
-    "ft/ht": "ft/h",
-    "1/30 deg/m": "1/30 dega/m",
-    "1000 ft.lbf": "1000 lbf.ft",
-    "1000 lbf": "klbf"
+    'deg': 'dega',
+    'lbm/gal': 'lbf/gal[US]',
+    'gal/min': 'gal[US]/min',
+    'c/min': 'rpm',
+    'kft.lbf': '1000 lbf.ft',
+    'deg/100ft': '0.01 dega/ft',
+    'feet': 'ft',
+    'FT': 'ft',
+    'foot': 'ft',
+    'ft/ht': 'ft/h',
+    '1/30 deg/m': '1/30 dega/m',
+    'deg/30m': '1/30 dega/m',
+    'deg/m': 'dega/m',
+    '1000 ft.lbf': '1000 lbf.ft',
+    '1000 lbf': 'klbf',
+    'kkgf': 'Mgf',
+    'ppg': 'lbm/gal[US]',
+    'lbm/gal': 'lbm/gal[US]',
+    '1000 kgf': 'Mgf',
+    'KKGF': 'Mgf',
+    'deg C': 'degC',
+    'RPM': 'rpm',
+    'KLBF': 'klbf',
+    'KFLB': 'klbf',
+    'G': 'gn',
+    "G's": 'gn',
+    'hr': 'h'
 }
 
 
