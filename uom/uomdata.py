627ac0423177d20f1782d135c54d978c0db86e3a
VV <velizar.vesselinov@gmail.com>
2022-04-27 11:19:32 -0500

    Added --extra-builtin=pkg_resources

--- uom/uomdata.py
+++ uom/uomdata.py
@@ -1,5 +1,6 @@
 """UOM DataFrame."""
-from pandas import read_csv
 from pkg_resources import resource_filename
+
+from pandas import read_csv
 
 DF_UOM = read_csv(resource_filename('uom', 'uom.csv'), index_col=0)
