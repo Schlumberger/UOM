7013f1dd62ba2a2d0a3d7ce5f8905d8fdb0f71e6
VV <velizar.vesselinov@gmail.com>
2022-04-28 08:06:51 -0500

    scrut

--- uom/__init__.py
+++ uom/__init__.py
@@ -4,4 +4,4 @@ from __future__ import absolute_import
 from .unit_alias import unit_alias
 from .uom import base_unit, conversion_factors, convert
 
-__all__ = [unit_alias, base_unit, conversion_factors, convert]
+__all__ = ['unit_alias', 'base_unit', 'conversion_factors', 'convert']
